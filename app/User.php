72d9498e05e398412e6c59f65bb7ddf508b41c1b
Fredy D Arce <fredyarce452@gmail.com>
2019-10-06 09:55:59 -0300

    Commit inicial y creacion de tablas, usuario y perfil

---
+++ app/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    //Relacion del usuario con el perfil
+    //Un usuario solo puede tener un perfil
+    public function profile(){//El metodo profile es en singular ya que un usuario va a tener un solo perfil
+      return $this->hasOne(Profile::class);
+    }
+}
