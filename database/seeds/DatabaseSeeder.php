4d7e3b258b29f4ca6c62bbc7fa94821dafb1376a
Fredy D Arce <fredyarce452@gmail.com>
2019-10-08 12:26:34 -0300

    Creando datos del usuario

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use App\Group;
+use App\Profile;
+use App\Location;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,5 +21,31 @@ class DatabaseSeeder extends Seeder
         factory(App\Level::class)->create(['name' => 'Plata']);
         factory(App\Level::class)->create(['name' => 'Bronce']);
 
+        //Creando datos del usuario
+        factory(App\User::class, 5)->create()->each(function ($user) {
+
+          $profile = $user->profile()->save(factory(App\Profile::class)->make());//Guardamos un perfil por cada usuario que creamos
+
+          $profile->location()->save(factory(App\Location::class)->make());//Por cada perfil tambien creamos un array de localizacion
+
+          $user->groups()->attach($this->array(rand(1,3)));//El metodo array nos debuelve un numero del 1 al 3 para seleccionar un grupo
+
+          $user->image()->save(factory(App\Location::class)->make([//guardamos una imagen de forma personalizada por cada usuario
+            'url' => 'https://lorempixel.com/90/90/'
+          ]));
+
+        });
+
+    }
+
+    public function array($max)  { //Metodo array que debuelve un valor
+
+      $values = [];
+
+      for ($i=1; $i < $max ; $i++) {
+        $values[] = $i;
+      }
+
+      return $values;
     }
 }
